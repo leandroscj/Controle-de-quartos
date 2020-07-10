4e93962aa20eff55654412dde93406a9471ae228
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-10 02:26:35 -0300

    Add project files.

---
+++ Controle de quartos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Controle_de_quartos
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Quartos[] vect = new Quartos[10];
+
+            Console.Write("Quantos quartos você deseja? ");
+            int qteQuartos = int.Parse(Console.ReadLine());
+
+
+
+
+
+            for (int i = 1; i <= qteQuartos; ++i)
+            {
+                Console.WriteLine("Aluguel #" + i);
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Numero do quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Quartos(nome, email);
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Controle de quartos/Quartos.cs
@@ -0,0 +1,17 @@
+﻿namespace Controle_de_quartos
+{
+    class Quartos
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Quartos(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return "Cliente: " + Nome + ", Email: " +  Email;
+        }
+    }
+}
